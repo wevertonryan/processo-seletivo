b43d277d2c63bd2d85b3d4be9319d9f226ea8d71
wevertonryan <wevertonryan205@gmail.com>
2025-04-08 22:40:08 -0300

    commit botão "ver senha" que aparece apenas quando tiver algum caracter, e desaparece caso não tenha nada no input

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-//função para permitir visibilidade da senha por um botão
-const ver_senha = document.getElementById("ver-senha");
-ver_senha.addEventListener("click", verSenha);
+//criação do botão "ver senha" quando tiver ao menos 1 caracter
+const senha = document.getElementById("senha");
+senha.addEventListener("input", (event) => {
+  const verSenhaElemento = document.getElementById("ver-senha"); //pega o botão "ver senha" caso ele exista
 
+  //caso não exista botão e exista um valor no input cria um novo botão
+  if (!verSenhaElemento && event.target.value != "") {
+    const verSenhaBotao = "<button id='ver-senha' onclick='verSenha()'>Olhinho</button>";
+    senha.insertAdjacentHTML("afterend", verSenhaBotao);
+
+  //caso exista um botão e não tenha nem um caracter deleta botão existente
+  } else if (event.target.value == "") {
+    ver_senha.parentNode.removeChild(ver_senha);
+    senha.type = "password"; //toda vez que apaga tudo desabilita visibilidade
+  }
+});
+
+//função chamada pelo botão "ver senha" para permitir visibilidade da senha
 function verSenha() {
-  const senha = document.getElementById("senha");
   if (senha.type == "password") {
     senha.type = "text";
   } else {
