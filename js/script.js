3706e20635eb17240612790f1291f41467e2f2eb
wevertonryan <wevertonryan205@gmail.com>
2025-04-09 00:14:46 -0300

    commit adição de css para deixar bonitão

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@
 const senha = document.getElementById("senha");
 senha.addEventListener("input", (event) => {
   const verSenhaElemento = document.getElementById("ver-senha"); //pega o botão "ver senha" caso ele exista
+  const verSenhaBotao = "<button id='ver-senha' onclick='verSenha()'>Olhinho</button>";
 
   //caso não exista botão e exista um valor no input cria um novo botão
   if (!verSenhaElemento && event.target.value != "") {
-    const verSenhaBotao = "<button id='ver-senha' onclick='verSenha()'>Olhinho</button>";
     senha.insertAdjacentHTML("afterend", verSenhaBotao);
 
   //caso exista um botão e não tenha nem um caracter deleta botão existente
   } else if (event.target.value == "") {
-    ver_senha.parentNode.removeChild(ver_senha);
+    verSenhaBotao.parentNode.removeChild(verSenhaBotao);
     senha.type = "password"; //toda vez que apaga tudo desabilita visibilidade
   }
 });
