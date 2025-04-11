f576fcd2b45d145dae13ec922ba06bfee759472b
wevertonryan <wevertonryan205@gmail.com>
2025-04-10 21:20:28 -0300

    Commit organização Script e de botão visibilidade, arquivando projetos futuros

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
-//Funções do Código:
-//- botão que permite a visibilidade da senha e que tire taambém
-//- permitir que o botão de visão apareça apenas quando tiver ao menos um caracter
-//- altere visualmente a cor
+/*---------------------------- FUNÇÕES DO CÓDIGO --------------------------------------
 
-//está armazenando os vetores que simbolizam o botão "ver-senha" em uma "variavel" 
-const viewOn = `<svg id="viewOn" class="view" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+- botão que permite a visibilidade da senha
+- permitir que o botão de visibilidade apareça apenas quando tiver ao menos um caracter
+
+
+----------------------------------- VETORES -----------------------------------------*/
+
+/* Armazenando os vetores que simbolizam o botão "ver-senha" em uma Constante*/
+//Vetor que permite a visibilidade da senha
+const viewOn = `<svg id="viewOn" onclick='verSenha()' class="view" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
 <circle cx="15" cy="15" r="5" fill="currentColor"/>
 <path d="M26.25 15C26.25 15 25 5 15 5C5 5 3.75 15 3.75 15" stroke="currentColor" stroke-width="2"/>
 </svg>`
-const viewOff = `<svg "viewOff" class="view" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+//Vetor que inibe a visibildade da senha
+const viewOff = `<svg id="viewOff" onclick='verSenha()' class="view" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M26 15C26 15 24.7778 21 15 21C5.22222 21 4 15 4 15" stroke="currentColor" stroke-width="2"/>
 <path d="M5.1 18.8L5.9 18.2L4.7 16.6L3.9 17.2L5.1 18.8ZM1.9 18.7C1.45817 19.0314 1.36863 19.6582 1.7 20.1C2.03137 20.5418 2.65817 20.6314 3.1 20.3L1.9 18.7ZM3.9 17.2L1.9 18.7L3.1 20.3L5.1 18.8L3.9 17.2Z" fill="currentColor"/>
 <path d="M24.9 18.8L24.1 18.2L25.3 16.6L26.1 17.2L24.9 18.8ZM28.1 18.7C28.5418 19.0314 28.6314 19.6582 28.3 20.1C27.9686 20.5418 27.3418 20.6314 26.9 20.3L28.1 18.7ZM26.1 17.2L28.1 18.7L26.9 20.3L24.9 18.8L26.1 17.2Z" fill="currentColor"/>
@@ -16,27 +22,27 @@ const viewOff = `<svg "viewOff" class="view" width="30" height="30" viewBox="0 0
 <path d="M19.1134 21.2866L18.813 20.3304L20.7402 19.8124L21.0405 20.7686L19.1134 21.2866ZM21.9636 23.707C22.1295 24.235 21.8325 24.7791 21.3004 24.9221C20.7682 25.0652 20.2024 24.753 20.0365 24.225L21.9636 23.707ZM21.0405 20.7686L21.9636 23.707L20.0365 24.225L19.1134 21.2866L21.0405 20.7686Z" fill="currentColor"/>
 </svg>`
 
+
+//----------------------- ADIÇÃO/REMOÇÃO BOTÃO VISIBILIDADE ---------------------------//
+
 //criação do botão "ver senha" quando tiver ao menos 1 caracter
 const senha = document.getElementById("senha");
+const verSenhaBotao = document.getElementById("ver-senha");
+
 senha.addEventListener("input", (event) => {
-  const verSenhaElemento = document.getElementById("ver-senha"); //pega o botão "ver senha" caso ele exista
-  let verSenhaBotao = `<div id='ver-senha' onclick='verSenha()'>${viewOff}</div>`;
-
-  //caso não exista botão e exista um valor no input cria um novo botão
-  if (!verSenhaElemento && event.target.value != "") {
-    senha.insertAdjacentHTML("afterend", verSenhaBotao);
-
-  //caso exista um botão e não tenha nem um caracter deleta botão existente
+  if (event.target.value != "" && verSenhaBotao.textContent == "") {
+    verSenhaBotao.innerHTML = viewOff;
+  //quando não tem nem um valor no Input Senha 
   } else if (event.target.value == "") {
-    verSenhaBotao = document.getElementById("ver-senha");
-    verSenhaBotao.parentNode.removeChild(verSenhaBotao);
-    senha.type = "password"; //toda vez que apaga tudo desabilita visibilidade
+    verSenhaBotao.innerHTML = "";
+    senha.type = "password"; //quando o usuario apaga todos os digitos desabilita visibilidade
   }
 });
 
+//--------------------------------- VISIBILIDADE --------------------------------------//
+
 //função chamada pelo botão "ver senha" para permitir visibilidade da senha
-function verSenha(event) {
-  const verSenhaBotao = document.getElementById("ver-senha");
+function verSenha() {
   if (senha.type == "password") {
     senha.type = "text";
     verSenhaBotao.innerHTML = viewOn;
@@ -45,3 +51,5 @@ function verSenha(event) {
     verSenhaBotao.innerHTML = viewOff;
   }
 }
+
+//-------------------------------------------------------------------------------------//
